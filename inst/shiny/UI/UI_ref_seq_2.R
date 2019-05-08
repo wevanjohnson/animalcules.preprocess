4651ec724a8cd59863dd85d344bd345101bfa11b
hjfan527 <howard.j.fan@gmail.com>
2019-05-08 10:53:24 -0400

    reactive variables

--- inst/shiny/UI/UI_ref_seq_2.R
+++ inst/shiny/UI/UI_ref_seq_2.R
@@ -46,27 +46,33 @@ ui <- fluidPage(
       
       tabPanel("Phylum",
                checkboxGroupInput("phylumGroup", label = "Choose a phylum:"),
+               actionLink("phylum_selectall","select all"),
                actionButton("phylum_update","Update")
       ),
       
       tabPanel("Class",
                checkboxGroupInput("classGroup", label = "Choose a class:"),
+               actionLink("class_selectall","select all"),
                actionButton("class_update","Update")
       ),
       tabPanel("Order",
                checkboxGroupInput("orderGroup", label = "Choose an order:"),
+               actionLink("order_selectall","select all"),
                actionButton("order_update","Update")
       ),
       tabPanel("Family",
                checkboxGroupInput("familyGroup", label = "Choose a family:"),
+               actionLink("family_selectall","select all"),
                actionButton("family_update","Update")
       ),
       tabPanel("Genus",
                checkboxGroupInput("genusGroup", label = "Choose a genus:"),
+               actionLink("genus_selectall","select all"),
                actionButton("genus_update","Update")
       ),
       tabPanel("Species",
                checkboxGroupInput("speciesGroup", label = "Choose a species:"),
+               actionLink("species_selectall","select all"),
                actionButton("species_update","Update")
       )
     )
@@ -87,15 +93,22 @@ server <- function(input, output, session) {
    # 
   #})
   
+  p_list <- reactiveVal(character(0))
+  c_list <- reactiveVal(character(0))
+  o_list <- reactiveVal(character(0))
+  f_list <- reactiveVal(character(0))
+  g_list <- reactiveVal(character(0))
+  s_list <- reactiveVal(character(0))
+  
   observeEvent(input$kingdom_update,{
     k_input <- input$kingdomGroup
     
-    p_list <- character(0)
-    c_list <- character(0)
-    o_list <- character(0)
-    f_list <- character(0)
-    g_list <- character(0)
-    s_list <- character(0)
+    p_list.new <- character(0)
+    c_list.new <- character(0)
+    o_list.new <- character(0)
+    f_list.new <- character(0)
+    g_list.new <- character(0)
+    s_list.new <- character(0)
     
     for(k in k_input){
       children_list <- NULL
@@ -110,35 +123,42 @@ server <- function(input, output, session) {
         children_list.g <- children_list[which(children_list$childtaxa_rank == 'genus'),]
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
         
-        p_list <- append(p_list, children_list.p$childtaxa_name)
-        c_list <- append(c_list, children_list.c$childtaxa_name)
-        o_list <- append(o_list, children_list.o$childtaxa_name)
-        f_list <- append(f_list, children_list.f$childtaxa_name)
-        g_list <- append(g_list, children_list.g$childtaxa_name)
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        p_list.new <- append(p_list.new, children_list.p$childtaxa_name)
+        c_list.new <- append(c_list.new, children_list.c$childtaxa_name)
+        o_list.new <- append(o_list.new, children_list.o$childtaxa_name)
+        f_list.new <- append(f_list.new, children_list.f$childtaxa_name)
+        g_list.new <- append(g_list.new, children_list.g$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
     
-    updateCheckboxGroupInput(session, "phylumGroup", choices = p_list)
-    updateCheckboxGroupInput(session, "classGroup", choices = c_list)
-    updateCheckboxGroupInput(session, "orderGroup", choices = o_list)
-    updateCheckboxGroupInput(session, "familyGroup", choices = f_list)
-    updateCheckboxGroupInput(session, "genusGroup", choices = g_list)
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "phylumGroup", choices = p_list.new)
+    updateCheckboxGroupInput(session, "classGroup", choices = c_list.new)
+    updateCheckboxGroupInput(session, "orderGroup", choices = o_list.new)
+    updateCheckboxGroupInput(session, "familyGroup", choices = f_list.new)
+    updateCheckboxGroupInput(session, "genusGroup", choices = g_list.new)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    p_list(p_list.new)
+    c_list(c_list.new)
+    o_list(o_list.new)
+    f_list(f_list.new)
+    g_list(g_list.new)
+    s_list(s_list.new)
   })
   
   observeEvent(input$phylum_update,{
     p_input <- input$phylumGroup
     
-    c_list <- character(0)
-    o_list <- character(0)
-    f_list <- character(0)
-    g_list <- character(0)
-    s_list <- character(0)
+    c_list.new <- character(0)
+    o_list.new <- character(0)
+    f_list.new <- character(0)
+    g_list.new <- character(0)
+    s_list.new <- character(0)
     
     for(p in p_input){
       children_list <- NULL
-      Sys.sleep(1)
+      Sys.sleep(0.5)
       children_list <- children(p, db = 'ncbi')
       if(!is.null(children_list)){
         children_list <- children_list[[1]]
@@ -149,31 +169,38 @@ server <- function(input, output, session) {
         children_list.g <- children_list[which(children_list$childtaxa_rank == 'genus'),]
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
         
-        c_list <- append(c_list, children_list.c$childtaxa_name)
-        o_list <- append(o_list, children_list.o$childtaxa_name)
-        f_list <- append(f_list, children_list.f$childtaxa_name)
-        g_list <- append(g_list, children_list.g$childtaxa_name)
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        c_list.new <- append(c_list.new, children_list.c$childtaxa_name)
+        o_list.new <- append(o_list.new, children_list.o$childtaxa_name)
+        f_list.new <- append(f_list.new, children_list.f$childtaxa_name)
+        g_list.new <- append(g_list.new, children_list.g$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
     
-    updateCheckboxGroupInput(session, "classGroup", choices = c_list)
-    updateCheckboxGroupInput(session, "orderGroup", choices = o_list)
-    updateCheckboxGroupInput(session, "familyGroup", choices = f_list)
-    updateCheckboxGroupInput(session, "genusGroup", choices = g_list)
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "classGroup", choices = c_list.new)
+    updateCheckboxGroupInput(session, "orderGroup", choices = o_list.new)
+    updateCheckboxGroupInput(session, "familyGroup", choices = f_list.new)
+    updateCheckboxGroupInput(session, "genusGroup", choices = g_list.new)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    c_list(c_list.new)
+    o_list(o_list.new)
+    f_list(f_list.new)
+    g_list(g_list.new)
+    s_list(s_list.new)
   })
   
   observeEvent(input$class_update,{
     c_input <- input$classGroup
     
-    o_list <- character(0)
-    f_list <- character(0)
-    g_list <- character(0)
-    s_list <- character(0)
+    o_list.new <- character(0)
+    f_list.new <- character(0)
+    g_list.new <- character(0)
+    s_list.new <- character(0)
     
     for(c in c_input){
       children_list <- NULL
+      Sys.sleep(0.5)
       children_list <- children(c, db = 'ncbi')
       if(!is.null(children_list)){
         children_list <- children_list[[1]]
@@ -183,29 +210,34 @@ server <- function(input, output, session) {
         children_list.g <- children_list[which(children_list$childtaxa_rank == 'genus'),]
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
         
-        o_list <- append(o_list, children_list.o$childtaxa_name)
-        f_list <- append(f_list, children_list.f$childtaxa_name)
-        g_list <- append(g_list, children_list.g$childtaxa_name)
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        o_list.new <- append(o_list.new, children_list.o$childtaxa_name)
+        f_list.new <- append(f_list.new, children_list.f$childtaxa_name)
+        g_list.new <- append(g_list.new, children_list.g$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
     
-    updateCheckboxGroupInput(session, "orderGroup", choices = o_list)
-    updateCheckboxGroupInput(session, "familyGroup", choices = f_list)
-    updateCheckboxGroupInput(session, "genusGroup", choices = g_list)
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "orderGroup", choices = o_list.new)
+    updateCheckboxGroupInput(session, "familyGroup", choices = f_list.new)
+    updateCheckboxGroupInput(session, "genusGroup", choices = g_list.new)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    o_list(o_list.new)
+    f_list(f_list.new)
+    g_list(g_list.new)
+    s_list(s_list.new)
   })
   
   observeEvent(input$order_update,{
     o_input <- input$orderGroup
 
-    f_list <- character(0)
-    g_list <- character(0)
-    s_list <- character(0)
+    f_list.new <- character(0)
+    g_list.new <- character(0)
+    s_list.new <- character(0)
     
     for(o in o_input){
       children_list <- NULL
-      delay
+      Sys.sleep(0.5)
       children_list <- children(o, db = 'ncbi')
       if(!is.null(children_list)){
         children_list <- children_list[[1]]
@@ -214,25 +246,30 @@ server <- function(input, output, session) {
         children_list.g <- children_list[which(children_list$childtaxa_rank == 'genus'),]
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
         
-        f_list <- append(f_list, children_list.f$childtaxa_name)
-        g_list <- append(g_list, children_list.g$childtaxa_name)
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        f_list.new <- append(f_list.new, children_list.f$childtaxa_name)
+        g_list.new <- append(g_list.new, children_list.g$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
     
-    updateCheckboxGroupInput(session, "familyGroup", choices = f_list)
-    updateCheckboxGroupInput(session, "genusGroup", choices = g_list)
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "familyGroup", choices = f_list.new)
+    updateCheckboxGroupInput(session, "genusGroup", choices = g_list.new)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    f_list(f_list.new)
+    g_list(g_list.new)
+    s_list(s_list.new)
   })
   
   observeEvent(input$family_update,{
     f_input <- input$familyGroup
     
-    g_list <- character(0)
-    s_list <- character(0)
+    g_list.new <- character(0)
+    s_list.new <- character(0)
     
     for(f in f_input){
       children_list <- NULL
+      Sys.sleep(0.5)
       children_list <- children(f, db = 'ncbi')
       if(!is.null(children_list)){
         children_list <- children_list[[1]]
@@ -240,34 +277,52 @@ server <- function(input, output, session) {
         children_list.g <- children_list[which(children_list$childtaxa_rank == 'genus'),]
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
 
-        g_list <- append(g_list, children_list.g$childtaxa_name)
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        g_list.new <- append(g_list.new, children_list.g$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
     
-    updateCheckboxGroupInput(session, "genusGroup", choices = g_list)
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "genusGroup", choices = g_list.new)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    g_list(g_list.new)
+    s_list(s_list.new)
   })
   
   observeEvent(input$genus_update,{
     g_input <- input$genusGroup
     
-    s_list <- character(0)
+    s_list.new <- character(0)
     
     for(g in g_input){
       children_list <- NULL
+      Sys.sleep(0.5)
       children_list <- children(g, db = 'ncbi')
       if(!is.null(children_list)){
         children_list <- children_list[[1]]
         
         children_list.s <- children_list[which(children_list$childtaxa_rank == 'species'),]
         
-        s_list <- append(s_list, children_list.s$childtaxa_name)
+        s_list.new <- append(s_list.new, children_list.s$childtaxa_name)
       }
     }
 
-    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list)
+    updateCheckboxGroupInput(session, "speciesGroup", choices = s_list.new)
+    
+    s_list(s_list.new)
   })
+  
+  observeEvent(input$phylum_selectall,{
+    print(input$phylum_selectall)
+    if(input$phylum_selectall%%2 == 0){
+      updateCheckboxGroupInput(session,"phylumGroup",choices=p_list())
+    }
+    else{
+      updateCheckboxGroupInput(session,"phylumGroup",choices=p_list(), selected=p_list())
+    }
+  })
+  
+  
 }
 
 # Create Shiny app ----
